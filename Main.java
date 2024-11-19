0a7f45381f4a7514851f1cd902d56fdd47b6f591
Sebastian-P-H <s.hoebbel02@gmail.com>
2024-11-19 22:47:10 +0100

    Versuch durch push Konflikt zu erzeugen, erlernen Github

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Factory factory = new Factory();
+
+        Car car1 = new Car("blue", "BMW", 5);
+        Car car2 = new Car("red", "Audi", 3);
+        Car car3 = new Car("green", "VW", 3);
+
+        Car car4 = new Car("yellow", "Renault", 4);
+
+        //An dieser Stelle befindet sich nun ein Array
+        //Dieses Array wird mit einem Sortieralgorithmus sortiert
+        //Optimaler Code Yippie
+
+        factory.buildCar(car1);
+        factory.buildCar(car2);
+
+    }
+}
+
